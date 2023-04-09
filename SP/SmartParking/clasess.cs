2641b7ba45238a356bde314e928c353dfb827e09
allisteraniceto <allisteraniceto@gmail.com>
2023-04-08 18:04:31 -0700

    added necessary classes such as Point, Slot, ParkingMap, Sensors, Sensor to SmartParking template

--- SP/SmartParking/clasess.cs
+++ SP/SmartParking/clasess.cs
@@ -18,7 +18,36 @@
         public Beacon[] data { get; set; }
 
     }
-   
+    
+
+    public class Slot //for easch parking slot
+    {
+        public Point[] position { get; set; }
+    }
+
+    //class parking lot that has total
+    public class ParkingMap
+    {
+        public int Total { get; set; }
+        public Slot[] data { get; set; } //array of Slots
+    }
+
+
+    public class Point //coordinatess
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+    public class Sensor
+    { //have to put [] after class because it is a property (c# syntax)
+        public Point[] position { get; set; } //array of Points (4 points)
+    }
+    public class Sensors
+    {
+        public Sensor data { get; set; } //array of sensors
+        public Sensor total { get; set; } 
+    }
+
     public class Response
 
     {
